913a7bc4ff91885a4af2179a9955c27fe793dddb
Gadzzaa <popescurobert986@gmail.com>
2021-10-08 19:58:09 +0300

    Update Notifier + Code Reformat

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ const tmi = require("tmi.js");
 const requireJSON = require('json-easy-strip');
 const settings = requireJSON('./settings.json');
 var fs = require('fs');
+var package = require('./package.json')
 var exec = require('child_process').execFile;
+const {Octokit} = require("@octokit/core");
+const octokit2 = new Octokit({auth: `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`});
+
+
+(async function () {
+    const versionOnline = await octokit2.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: 'Gadzzaa',
+        repo: 'gadzzaaTwitchBot',
+        path: 'package.json'
+    });
+    let bufferObj = Buffer.from(versionOnline.data.content, "base64");
+    let decodedString = JSON.parse(bufferObj.toString("utf8"));
+    console.log(decodedString.version)
+    console.log(package.version)
+    if (decodedString.version < package.version) {
+        console.log('######## NEW UPDATE ########');
+        console.log('Current Version : ' + decodedString.version);
+        console.log('Newest Version : ' + package.version);
+        console.log('Date Released : ' + decodedString.dateReleased)
+        if (decodedString.bugfixes) {
+            console.log('This version includes bugfixes so i recommend you update ASAP')
+        }
+    }
+})();
 
 
 const twitchclient = new tmi.client(
@@ -22,7 +47,7 @@ const twitchclient = new tmi.client(
 
 function execute(fileName, params, path) {
     let promise = new Promise((resolve, reject) => {
-        exec(fileName, params, { cwd: path }, (err, data) => {
+        exec(fileName, params, {cwd: path}, (err, data) => {
             if (err) reject(err);
             else resolve(data);
         });
@@ -33,36 +58,41 @@ function execute(fileName, params, path) {
 
 twitchclient.connect();
 
-twitchclient.on("connected",(address,port) => {
-    execute('osu!StreamCompanion.exe',[],settings.StreamCompanion_Path)
+twitchclient.on("connected", (address, port) => {
+    execute('osu!StreamCompanion.exe', [], settings.StreamCompanion_Path)
     console.log("Starting StreamCompanion and NP Bot...")
-    setTimeout(function(){
-            if(settings.debug)
-            {
-                console.log("NP bot has started on port: " + port);
-            }else
-            {
-                console.log("NP bot has started");
-            }
-        },5000);
+    setTimeout(function () {
+        if (settings.debug) {
+            console.log("NP bot has started on port: " + port);
+        } else {
+            console.log("NP bot has started");
+        }
+    }, 5000);
 });
 
 
-
-twitchclient.on('message',(channel, context, message, self) => {
+twitchclient.on('message', (channel, context, message, self) => {
     let username = context["display-name"];
-    if (message.toLowerCase() === settings.Prefix + settings.Now_Playing_Command){
-        filename = settings.StreamCompanion_Path + '\\Files\\np_all.txt'
-        fs.readFile(filename, 'utf8', function(err, data) {
-        if (err) throw err;
-        twitchclient.say(channel,"/me " + data)
-     });
+    if (message.toLowerCase() === settings.Prefix + settings.Now_Playing_Command) {
+        filename = settings.StreamCompanion_Path + '\\Files\\' + settings.Now_Playing_Command + '.txt'
+        fs.readFile(filename, 'utf8', function (err, data) {
+            if (err) throw err;
+            if (settings.me) {
+                twitchclient.say(channel, "/me " + data)
+            } else {
+                twitchclient.say(channel, data)
+            }
+        });
     }
-    if (message.toLowerCase() === settings.Prefix + settings.Now_Playing_PP_Command){
-        filename = settings.StreamCompanion_Path + '\\Files\\nppp.txt'
-        fs.readFile(filename, 'utf8', function(err, data) {
-        if (err) throw err;
-        twitchclient.say(channel,"/me " + data)
-     });
+    if (message.toLowerCase() === settings.Prefix + settings.Now_Playing_PP_Command) {
+        filename = settings.StreamCompanion_Path + '\\Files\\' + settings.Now_Playing_PP_Command + '.txt'
+        fs.readFile(filename, 'utf8', function (err, data) {
+            if (err) throw err;
+            if (settings.me) {
+                twitchclient.say(channel, "/me " + data)
+            } else {
+                twitchclient.say(channel, data)
+            }
+        });
     }
 })
